3f0eab28a8af175939a3326ff2e0f4befb675b0e
Titovilal <scastellsjuan@gmail.com>
2024-01-19 21:09:24 +0100

    Add Sheet component to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,20 +9,35 @@ import {
   Typography,
   Divider,
   Input,
+  Sheet,
 } from "@mui/joy";
 
 export default function Header() {
   return (
     <>
-      <Typography level="h3">Downloads manager</Typography>
-      <Box sx={{ display: "flex", gap: "1rem" }}>
-        <IconButton sx={{ borderRadius: "lg" }} variant="outlined">
-          <ModeNightIcon />
-        </IconButton>
-        <IconButton sx={{ borderRadius: "lg" }} variant="outlined">
-          <SettingsIcon />
-        </IconButton>
-      </Box>
+      <Sheet
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          height: "100%",
+          px: "1rem",
+          bgcolor: "neutral.100",
+        }}
+      >
+        <Typography level="h3">Downloads manager</Typography>
+        <Box sx={{ display: "flex", gap: "1rem" }}>
+          <IconButton sx={{ borderRadius: "lg" }} variant="outlined">
+            EN
+          </IconButton>
+          <IconButton sx={{ borderRadius: "lg" }} variant="outlined">
+            <ModeNightIcon />
+          </IconButton>
+          <IconButton sx={{ borderRadius: "lg" }} variant="outlined">
+            <SettingsIcon />
+          </IconButton>
+        </Box>
+      </Sheet>
     </>
   );
 }
